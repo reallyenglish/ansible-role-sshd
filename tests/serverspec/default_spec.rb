82b9d5c25fe2ac8c751b8523e1890bbfe9b0ee5d
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-06-23 13:23:54 +0900

    QA

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -1,41 +1,44 @@
-require 'spec_helper'
+require "spec_helper"
 
-package = 'openssh-server'
-service = 'sshd'
-config  = '/etc/ssh/sshd_config'
-user    = 'sshd'
-group   = 'sshd'
-ports   = [ 22 ]
-sftp_server = '/usr/lib/sftp-server'
+package = "openssh-server"
+service = "sshd"
+config  = "/etc/ssh/sshd_config"
+ports   = [22]
+sftp_server = "/usr/lib/sftp-server"
+default_user = "root"
+default_group = "root"
 
 case os[:family]
-when 'freebsd', 'openbsd'
-  sftp_server = '/usr/libexec/sftp-server'
-when 'ubuntu'
-  service = 'ssh'
-when 'centos'
-  service = 'sshd.service'
+when "freebsd", "openbsd"
+  sftp_server = "/usr/libexec/sftp-server"
+  default_group = "wheel"
+when "ubuntu"
+  service = "ssh"
+when "redhat"
+  service = "sshd.service"
 end
 
-case os[:family]
-when 'freebsd', 'openbsd'
-else
+if os[:family] != "freebsd" && os[:family] != "openbsd"
   describe package(package) do
     it { should be_installed }
-  end 
+  end
 end
 
 describe file(config) do
+  it { should exist }
   it { should be_file }
-  its(:content) { should match /PermitRootLogin without-password/ }
-  its(:content) { should match /PasswordAuthentication no/ }
-  its(:content) { should match /UseDNS no/ }
-  its(:content) { should match /Subsystem sftp #{ sftp_server }/ }
+  it { should be_mode os[:family] == "redhat" ? 600 : 644 }
+  it { should be_owned_by default_user }
+  it { should be_grouped_into default_group }
+  its(:content) { should match(/^PermitRootLogin without-password$/) }
+  its(:content) { should match(/^PasswordAuthentication no$/) }
+  its(:content) { should match(/^UseDNS no$/) }
+  its(:content) { should match(/^Subsystem sftp #{ sftp_server }$/) }
   case os[:family]
-  when 'openbsd'
-    its(:content) { should_not match /UsePAM no/ }
+  when "openbsd"
+    its(:content) { should_not match(/^UsePAM no$/) }
   else
-    its(:content) { should match /UsePAM no/ }
+    its(:content) { should match(/^UsePAM no$/) }
   end
 end
 
